e55b2b15393e66c9b418a50d430bc553fc693ada
Matthew Aleshechkin <aleshechkin.matthew@gmail.com>
2021-11-30 13:58:55 -0600

    Added invalid dates

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 const fs = require("fs");
-
+// filter args we dont care about
 let args = process.argv.slice(2);
 
+// comare two days
+function sameDay(d1, d2) {
+  return d1.getFullYear() === d2.getFullYear() &&
+    d1.getMonth() === d2.getMonth() &&
+    d1.getDate() === d2.getDate();
+}
+
+// Read class parts
 let cont = fs.readFileSync(args[0]).toString().split("\r\n");
-let STARTDATE = new Date(2021, 8, 2);
+// read first date
+let begin = args[2].split(",")
+let STARTDATE = new Date(Number(begin[0]),Number(begin[1]) - 1,Number(begin[2]));
+
+// Read illegal dates
+let content = fs.readFileSync(args[1]).toString().split("\r\n");
+var invalidDays = [];
+
+for(let x = 0; x < content.length; x++){
+  x = content[x]
+  x = x.split(",")
+  let date = new Date(Number(x[0]),Number(x[1]) - 1,Number(x[2]))
+  invalidDays.push(date)
+}
 
 var days = Object();
-
+// read days 
 for (let x = 0; x < cont.length; x++) {
   let line = cont[x];
   let daynum = Number(line.split(",", 1)[0]);
@@ -17,7 +38,7 @@ for (let x = 0; x < cont.length; x++) {
 var lastday = 0;
 
 var coursedays = [];
-
+// place days ignoring sunday, saturday and holidays
 for (var daynum in days) {
   if (lastday == 0) {
     coursedays.push({
@@ -32,7 +53,15 @@ for (var daynum in days) {
     if (STARTDATE.getDay() == 0 || STARTDATE.getDay() == 6) {
       continue;
     }
-    break;
+    let next = false
+    for (let day = 0; day < invalidDays.length; day++){
+      if(sameDay(STARTDATE, invalidDays[day])){
+        next = true
+      }
+    }
+    if(!next){
+      break;
+    }
   }
   coursedays.push({
     date: JSON.parse(JSON.stringify(STARTDATE)),
